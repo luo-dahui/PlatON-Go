a3ced075ff0c09df43a63b820ca20abb9e0b39f6
luodahui <934803813@qq.com>
2020-03-20 17:11:59 +0800

    update

--- cmd/platon_tool/core/transactioncmd.go
+++ cmd/platon_tool/core/transactioncmd.go
@@ -1,13 +1,16 @@
 package core
 
 import (
+	"bytes"
 	"crypto/ecdsa"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"math/big"
+	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/PlatONnetwork/PlatON-Go/accounts/keystore"
 	"github.com/PlatONnetwork/PlatON-Go/cmd/utils"
@@ -46,23 +49,96 @@ func getTxReceiptCmd(c *cli.Context) {
 		hash = config.Call.TxHash
 	}
 
-	GetTxReceipt(hash)
+	receipt, reCode, _ := GetTxReceipt(hash, nil, nil, false)
+
+	fmt.Printf("reCode:%s\n", reCode)
+	if reCode == "0" {
+		fmt.Println("经济模型交易成功.")
+	} else if reCode == "00" {
+		fmt.Println("普通转账交易成功.")
+	} else {
+		fmt.Println("交易失败.")
+	}
+
+	fmt.Println("========================")
+	out, _ := json.MarshalIndent(receipt, "", "  ")
+	fmt.Println(string(out))
 }
 
-func GetTxReceipt(txHash string) (Receipt, error) {
+// 经济模型合约地址
+var listAddress = []string{
+	"0x1000000000000000000000000000000000000002",
+	"0x1000000000000000000000000000000000000003",
+	"0x1000000000000000000000000000000000000005",
+	"0x1000000000000000000000000000000000000004",
+	"0x1000000000000000000000000000000000000001",
+	"0x1000000000000000000000000000000000000006",
+}
+
+func IsEcoModuleAddress(to string) bool {
+	for _, eachItem := range listAddress {
+		if eachItem == to {
+			return true
+		}
+	}
+	return false
+}
+
+func GetTxReceipt(txHash string, ch chan string, exit chan string, wait bool) (Receipt, string, error) {
 	var receipt = Receipt{}
-	res, _ := Send([]string{txHash}, "platon_getTransactionReceipt")
-	e := json.Unmarshal([]byte(res), &receipt)
-	if e != nil {
-		panic(fmt.Sprintf("parse get receipt result error ! \n %s", e.Error()))
+	// 0:经济模型交易成功; 00:普通交易成功;
+	retCode := "0"
+	for {
+		res, _ := Send([]string{txHash}, "platon_getTransactionReceipt")
+		e := json.Unmarshal([]byte(res), &receipt)
+		if e != nil {
+			panic(fmt.Sprintf("parse get receipt result error ! \n %s", e.Error()))
+		}
+
+		BlockHash := receipt.Result.BlockHash
+		if BlockHash != "" {
+			// 判断是否是经济模型交易
+			if IsEcoModuleAddress(receipt.Result.To) == true {
+				logs := receipt.Result.Logs
+				if len(logs) != 0 {
+					var args [][]byte
+					if err := rlp.Decode(bytes.NewReader(logs[0].Data), &args); nil != err {
+						fmt.Fprintln(os.Stderr, err)
+						os.Exit(1)
+					}
+					retCode = string(args[0])
+				} else {
+					// 经济模型交易失败
+					retCode = "1"
+				}
+			} else {
+				// 普通交易成功
+				retCode = "00"
+			}
+
+			// 只查询一次
+			if wait == false {
+				break
+			}
+
+			ch <- retCode
+			break
+		}
+
+		select {
+		case <-exit:
+			break
+		default:
+		}
 	}
 
 	if receipt.Result.BlockHash == "" {
 		panic("no receipt found")
 	}
-	out, _ := json.MarshalIndent(receipt, "", "  ")
-	fmt.Println(string(out))
-	return receipt, nil
+	/*
+		out, _ := json.MarshalIndent(receipt, "", "  ")
+		fmt.Println(string(out))*/
+	return receipt, retCode, nil
 }
 
 func sendTransactionCmd(c *cli.Context) error {
@@ -86,13 +162,37 @@ func sendTransactionCmd(c *cli.Context) error {
 		utils.Fatalf("Send transaction error: %v", err)
 	}
 
-	fmt.Printf("tx hash: %s", hash)
+	fmt.Printf("tx hash: %s \n", hash)
+	fmt.Println("==========please wait=============")
+
+	// Get transaction receipt according to result
+	ch := make(chan string, 1)
+	exit := make(chan string, 1)
+	go GetTxReceipt(hash, ch, exit, true)
+
+	/*
+	  Loop call to get transactionReceipt... until 200s timeout
+	*/
+	select {
+	case retCode := <-ch:
+		fmt.Printf("retCode: %s\n", retCode)
+		if retCode == "0" {
+			fmt.Println("transaction succeed.\n")
+		} else {
+			fmt.Println("transaction failed.\n")
+		}
+	case <-time.After(time.Second * 200):
+		exit <- "exit"
+		fmt.Printf("get transaction receipt timeout...more than 200 second.\n")
+	}
+
 	return nil
 }
 
 func sendRawTransactionCmd(c *cli.Context) error {
 
 	parseConfigJson(c.String(ConfigPathFlag.Name))
+
 	walletFile := c.String(WalletFilePathFlag.Name)
 	if walletFile == "" {
 		walletFile = config.Tx.Wallet
@@ -122,12 +222,35 @@ func sendRawTransactionCmd(c *cli.Context) error {
 		value = config.Tx.Value
 	}
 
+	// 发送交易
 	hash, err := SendRawTransaction(from, to, value, key.PrivateKey)
 	if err != nil {
 		utils.Fatalf("Send transaction error: %v", err)
 	}
 
-	fmt.Printf("tx hash: %s", hash)
+	fmt.Printf("tx hash: %s \n", hash)
+	fmt.Println("==========please wait=============")
+
+	// Get transaction receipt according to result
+	ch := make(chan string, 1)
+	exit := make(chan string, 1)
+	go GetTxReceipt(hash, ch, exit, true)
+
+	/*
+	  Loop call to get transactionReceipt... until 200s timeout
+	*/
+	select {
+	case retCode := <-ch:
+		fmt.Printf("retCode: %s\n", retCode)
+		if retCode == "0" {
+			fmt.Println("transaction succeed.\n")
+		} else {
+			fmt.Println("transaction failed.\n")
+		}
+	case <-time.After(time.Second * 200):
+		exit <- "exit"
+		fmt.Printf("get transaction receipt timeout...more than 200 second.\n")
+	}
 	return nil
 }
 
@@ -205,8 +328,31 @@ func sendRawTransaction(transaction *types.Transaction) (string, error) {
 func getSignedTransaction(from, to string, value int64, priv *ecdsa.PrivateKey,
 	nonce uint64) *types.Transaction {
 
-	gas, _ := strconv.Atoi(config.Tx.Gas)
-	gasPrice, _ := new(big.Int).SetString(config.Tx.GasPrice, 10)
+	var gas uint64
+	var err error
+	if config.Tx.Gas[0:2] == "0x" {
+		bigGas, ok := new(big.Int).SetString(config.Tx.Gas[2:], 16)
+		if ok == false || bigGas == nil {
+			panic(fmt.Errorf("big Gas is error"))
+		}
+		gas = bigGas.Uint64()
+	} else {
+		intGas, _ := strconv.Atoi(config.Tx.Gas)
+		gas = uint64(intGas)
+	}
+
+	var gasPrice *big.Int
+	if config.Tx.GasPrice[0:2] == "0x" {
+		bigGasPrice, ok := new(big.Int).SetString(config.Tx.GasPrice[2:], 16)
+		if ok == false || bigGasPrice == nil {
+			panic(fmt.Errorf("big gasPrice is error"))
+		}
+		gasPrice = bigGasPrice
+	} else {
+		bigGasPrice, _ := new(big.Int).SetString(config.Tx.GasPrice, 10)
+		gasPrice = bigGasPrice
+	}
+
 	newTx, err := types.SignTx(types.NewTransaction(nonce, common.HexToAddress(to),
 		big.NewInt(value), uint64(gas), gasPrice, []byte{}),
 		types.NewEIP155Signer(config.ChainID), priv)
